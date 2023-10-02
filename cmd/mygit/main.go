2b2360d5cf53a6ead63d1c018ce79a9d1e9eb1a9
Maxwell D. Dorliea <deboy170@gmail.com>
2023-10-02 01:26:14 +0000

    Implement the cat-file command with flag '-p'

--- cmd/mygit/main.go
+++ cmd/mygit/main.go
@@ -1,11 +1,48 @@
 package main
 
 import (
+	"bytes"
+	"compress/zlib"
+	"errors"
 	"fmt"
+	"io"
 	"os"
 )
 
 // Usage: your_git.sh <command> <arg1> <arg2> ...
+
+func GitInit() error {
+	for _, dir := range []string{".git", ".git/objects", ".git/refs"} {
+		if err := os.MkdirAll(dir, 0755); err != nil {
+			return errors.New(fmt.Sprintf("Error creating directory: %s\n", err))
+		}
+	}
+	headFileContents := []byte("ref: refs/heads/master\n")
+	if err := os.WriteFile(".git/HEAD", headFileContents, 0644); err != nil {
+		return errors.New(fmt.Sprintf("Error writing file: %s\n", err))
+	}
+	fmt.Println("Initialized git directory")
+	return nil
+}
+
+func GitCatFile(hash, flag string) error {
+	filePath := fmt.Sprintf(".git/objects/%s/%s", hash[:2], hash[2:])
+	file, err := os.ReadFile(filePath)
+	if err != nil {
+		return errors.New(fmt.Sprintf("Error reading file: %s\n", err))
+	}
+	b := bytes.NewReader(file)
+	data, err := zlib.NewReader(b)
+	if err != nil {
+		return errors.New(fmt.Sprintf("Error uncompressing file: %s\n", err))
+	}
+	if flag == "-p" {
+		io.Copy(os.Stdout, data)
+		data.Close()
+	}
+	return nil
+}
+
 func main() {
 	fmt.Println("Logs from your program will appear here!")
 
@@ -16,19 +53,15 @@ func main() {
 
 	switch command := os.Args[1]; command {
 	case "init":
-		for _, dir := range []string{".git", ".git/objects", ".git/refs"} {
-			if err := os.MkdirAll(dir, 0755); err != nil {
-				fmt.Fprintf(os.Stderr, "Error creating directory: %s\n", err)
-			}
+		err := GitInit()
+		if err != nil {
+			fmt.Fprint(os.Stderr, err.Error())
 		}
-
-		headFileContents := []byte("ref: refs/heads/master\n")
-		if err := os.WriteFile(".git/HEAD", headFileContents, 0644); err != nil {
-			fmt.Fprintf(os.Stderr, "Error writing file: %s\n", err)
+	case "cat-file":
+		err := GitCatFile(os.Args[3], os.Args[2])
+		if err != nil {
+			fmt.Fprint(os.Stderr, err.Error())
 		}
-
-		fmt.Println("Initialized git directory")
-
 	default:
 		fmt.Fprintf(os.Stderr, "Unknown command %s\n", command)
 		os.Exit(1)
